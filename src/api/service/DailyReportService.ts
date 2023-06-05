508bb656777b5711d782749bc0d13f90dcaf6dfe
eGangotri <egangotritrust@gmail.com>
2023-06-05 23:39:27 +0530

    refresh

--- src/api/service/DailyReportService.ts
+++ src/api/service/DailyReportService.ts
@@ -3,71 +3,63 @@ import { callBackendGetApi, callBackendGetApiForBlob, callBackendPostApi } from 
 import { backendServer } from "api/constants";
 
 
-  
-export async function sendFilteredFormToServerGet(operators:string,centers:string, selectedStartDate:Date|null, selectedEndDate:Date|null) {
-  
+
+export async function sendFilteredFormToServerGet(operators: string, centers: string, selectedStartDate: string | null, selectedEndDate: string | null) {
     const params = {
-      operators,
-      centers,
-      selectedStartDate,
-      selectedEndDate
+        operatorName: operators,
+        centers,
+        startDate: selectedStartDate,
+        endDate: selectedEndDate
     }
-  
     const resp = await callBackendGetApiForBlob("dailyWorkReport/csvAsFile", params);
     console.log(`res ${JSON.stringify(resp)}`)
     return resp
-  }
-  
+}
 
 export async function pushReportToServer(dailyReport: DailyWorkReportType, password: string): Promise<AddDailyReportResponseType> {
-
     const _reportBody = {
-      ...dailyReport,
-      password
+        ...dailyReport,
+        password
     }
-  
+
     const resp = await callBackendPostApi("dailyWorkReport/add", _reportBody);
     console.log(`res ${JSON.stringify(resp)}`)
     const respAsJson = await resp.json()
     console.log(`respAsJson ${JSON.stringify(respAsJson)}`)
     return respAsJson
-  }
-  
-  export async function sendFilteredFormToServerPost(operators:string,centers:string, selectedStartDate:Date|null, selectedEndDate:Date|null) {
-  
+}
+
+export async function sendFilteredFormToServerPost(operators: string, centers: string, selectedStartDate: Date | null, selectedEndDate: Date | null) {
+
     const _reportBody = {
-      operators,
-      centers,
-      selectedStartDate,
-      selectedEndDate
+        operators,
+        centers,
+        selectedStartDate,
+        selectedEndDate
     }
-  
+
     const resp = await callBackendPostApi("dailyWorkReport/csvAsFile", _reportBody);
     console.log(`res ${JSON.stringify(resp)}`)
     const respAsJson = await resp.json()
     console.log(`respAsJson ${JSON.stringify(respAsJson)}`)
     return respAsJson
-  }
-  
-  export async function checkValidCredentials(staffName: string, password: string):Promise<LoginProps> {
+}
+
+export async function checkValidCredentials(staffName: string, password: string): Promise<LoginProps> {
     const loginInfo = {
-      username: staffName,
-      password: password
+        username: staffName,
+        password: password
     } as LoginUser;
-  
-    const _url = `${backendServer}user/checkValidCredentials`
-    console.log(`_url ${_url}`)
-  
+
+
     const options: RequestInit = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(loginInfo),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(loginInfo),
     }
-  
-    const res = await fetch(_url, options)
-    //shall be  {"response":false} or {"response":true}
+
+    const res = await callBackendPostApi("user/checkValidCredentials", loginInfo)
     const items = await res.json()
     console.log(`checkValidCredentials:res ${JSON.stringify(items)}`)
     return items
-  }
-  
+}
--- src/pages/DeliverableReports/index.tsx
+++ src/pages/DeliverableReports/index.tsx
@@ -17,7 +17,9 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateRange } from '@mui/x-date-pickers-pro';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import { sendFilteredFormToServerGet, sendFilteredFormToServerPost } from "api/service/DailyReportService";
-import { BASIC_ROLE } from "utils/DailyReportUtil";
+import _ from "lodash";
+import moment from "moment";
+import { DD_MM_YYYY_FORMAT } from "utils/DailyReportUtil";
 
 
 const DeliverableReports = () => {
@@ -28,31 +30,39 @@ const DeliverableReports = () => {
     const [operators, setOperators] = useState("");
     const [centers, setCenters] = useState("");
 
-    const [selectedStartDate, setSelectedStartDate] = React.useState<Date | null>(null);
-    const [selectedEndDate, setSelectedEndDate] = React.useState<Date | null>(null);
+    const [selectedStartDate, setSelectedStartDate] = React.useState<string | null>(null);
+    const [selectedEndDate, setSelectedEndDate] = React.useState<string | null>(null);
 
     const filterReport = () => {
         console.log(`filterReport`);
         sendFilteredFormToServerGet(operators, centers, selectedStartDate, selectedEndDate)
     }
-    const [value, setValue] = React.useState<DateRange<Dayjs | null>>([
+
+    const [dayRangeValue, setDayRangeValue] = React.useState<DateRange<Dayjs | null>>([
         dayjs(new Date()),
         dayjs(new Date()),
     ]);
 
     const clearResults = () => {
-        setIsLoggedIn(false);
-        setLoggedUser(""); 
-        setLoggedUserRole(BASIC_ROLE);
+        console.log("clearResults");
+        setOperators("")
+        setCenters("")
+        setDayRangeValue([
+            dayjs(null),
+            dayjs(null),
+        ]);
+        setSelectedStartDate(null);
+        setSelectedEndDate(null);
     };
 
 
-    const onDatePickerChange = (newValue: DateRange<Dayjs>) => {
-        console.log(`newValue ${newValue[0]}`)
-        setSelectedStartDate(newValue[0]?.toDate() || null);
-        console.log(`newValue ${newValue[1]}`)
-        setSelectedEndDate(newValue[1]?.toDate() || null);
-        setValue(newValue);
+    const onDatePickerChange = (newDayRangeValue: DateRange<Dayjs>) => {
+        console.log(`newValue ${newDayRangeValue[0]}`)
+        const _newValue = moment(newDayRangeValue[0]?.toDate());
+        setSelectedStartDate(_newValue.format(DD_MM_YYYY_FORMAT));
+        console.log(`newValue ${newDayRangeValue[1]}`)
+        setSelectedEndDate(moment(newDayRangeValue[1]?.toDate()).format(DD_MM_YYYY_FORMAT) || null);
+        setDayRangeValue(newDayRangeValue);
     }
 
     return (
@@ -70,6 +80,7 @@ const DeliverableReports = () => {
                                 variant="outlined"
                                 label="Required"
                                 size="small"
+                                value={operators}
                                 onChange={(e) => setOperators(e.target.value)}
                             />
                         </Box>
@@ -80,6 +91,7 @@ const DeliverableReports = () => {
                                 variant="outlined"
                                 label="Required"
                                 size="small"
+                                value={centers}
                                 onChange={(e) => setCenters(e.target.value)}
                             />
                         </Box>
@@ -88,7 +100,7 @@ const DeliverableReports = () => {
                             <DemoContainer components={['DateRangePicker', 'DateRangePicker']}>
                                 <DemoItem label="Filter by Time: " component="DateRangePicker">
                                     <DateRangePicker
-                                        value={value}
+                                        value={dayRangeValue}
                                         onChange={(newValue: DateRange<Dayjs>) => onDatePickerChange(newValue)}
                                     />
                                 </DemoItem>
